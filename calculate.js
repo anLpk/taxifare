7b2bd7c408e65946da6db0ed2bbb7a744ae678b8
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-09-16 21:29:30 +1000

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,35 @@
+function multiplyBy()
+{
+  if (document.getElementById("istanbul").checked)
+  {
+    price = document.getElementById("distance").value * 3.1 + 5;
+    document.getElementById("result").innerHTML = (price).toFixed(2) + " Turkish Lira with Yellow Taxi";
+
+    time = document.getElementById("distance").value * 1.3;
+    document.getElementById("time").innerHTML = (time).toFixed(1) + " minutes";
+
+  } else if (document.getElementById("ankara").checked) {
+    price = document.getElementById("distance").value * 3.4 + 4.6;
+    document.getElementById("result").innerHTML = (price).toFixed(2) + " Turkish Lira with Yellow Taxi";
+
+    time = document.getElementById("distance").value * 1.2;
+    document.getElementById("time").innerHTML = (time).toFixed(1) + " minutes";
+
+  } else if (document.getElementById("izmir").checked) {
+    price = document.getElementById("distance").value * 3.7 + 3;
+    document.getElementById("result").innerHTML = (price).toFixed(2) + " Turkish Lira with Yellow Taxi";
+
+    time = document.getElementById("distance").value * 1.1;
+    document.getElementById("time").innerHTML = (time).toFixed(1) + " minutes";
+  } else {
+    document.getElementById("result").innerHTML = "No one selected !"
+  }
+}
+
+function scaleFun() {
+  document.getElementById("logo").style = "transform: rotate(10deg);"
+}
+
+btn.addEventListener('click', (event) => {
+  event.target.textContent = 'Thank You!';
+});
